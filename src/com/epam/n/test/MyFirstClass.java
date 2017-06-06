a0ff23a19671dae7b705da8e9753e304affa8551
Nataliya-G <party9@yandex.ru>
2017-06-07 00:52:53 +0600

    Initial commit

---
+++ src/com/epam/n/test/MyFirstClass.java
@@ -0,0 +1,11 @@
+package com.epam.n.test;
+
+/**
+ * Created by User on 07.06.2017.
+ */
+public class MyFirstClass {
+    public static void main(String[] args) {
+        String a= "Привет";
+        System.out.println(a);
+    }
+}
